6055086b4a758080c89664a2473fd31ddcf973b4
Atsushi Hirata <atsushi.hirata890@gmail.com>
2016-01-31 20:49:48 -0500

    reworked the func that communicates with API to get all transactions. Created callback on the func so async call computes properly. Starting to populate the cell with transaction/item information.

---
+++ adae-ios-atsushi/TransactionTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  TransactionTableViewCell.swift
+//  adae-ios-atsushi
+//
+//  Created by Atsushi Hirata on 2016-01-30.
+//  Copyright © 2016 adae. All rights reserved.
+//
+
+import UIKit
+
+class TransactionTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    
+    @IBOutlet weak var item_image: UIImageView!
+    
+    @IBOutlet weak var avatar: UIImageView!
+    
+    @IBOutlet weak var seller_name: UILabel!
+    
+    @IBOutlet weak var description_text: UILabel!
+    
+    @IBOutlet weak var cancel_button: UIButton!
+    
+    @IBOutlet weak var time_frame: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+}
--- adae-ios-atsushi/TransactionViewController.swift
+++ adae-ios-atsushi/TransactionViewController.swift
@@ -16,9 +16,17 @@ class TransactionViewController: UITableViewController {
     
     let myarray = ["item1", "item2", "item3"]
     
-    var images = [String]()
+    /**var images = [JSON]()
     
-    var items = [String]()
+    var items = [JSON]()
+    
+    var users = [JSON]()**/
+    
+    var jsonObject: JSON = [
+        ["name": "John", "age": 21],
+        ["name": "Bob", "age": 35],
+    ]
+
     
     let page_number = 1
     
@@ -27,13 +35,16 @@ class TransactionViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getTransactions { (isOk) -> Void in
+        self.getTransactions { (isOk) -> Void in
             if (isOk) {
+                
+                self.tableview.reloadData()
+
                 print("async success")
+
             }else{
                 print("async fail")
             }
-
         }
         
         print("Inside of Transaction Controller")
@@ -41,7 +52,7 @@ class TransactionViewController: UITableViewController {
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        tableview.reloadData()
+        //tableview.reloadData()
     }
     
     override func didReceiveMemoryWarning() {
@@ -49,27 +60,28 @@ class TransactionViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        return jsonObject["transaction"].count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCellWithIdentifier("transactionCell", forIndexPath: indexPath) //as! UITableViewCell
-        //cell.textLabel?.text = myarray[indexPath.item]
-        print("!!!!!!!!!!!!!!!!!!!!!!")
-        print(self.images)
-        print(indexPath.item)
+        let cell = tableView.dequeueReusableCellWithIdentifier("transactionCell", forIndexPath: indexPath) as! TransactionTableViewCell
         
+        cell.description_text.text = String(self.jsonObject["item"][indexPath.item]["description"])
         
-        /**cell.imageView?.contentMode = UIViewContentMode.ScaleAspectFill
-        cell.imageView?.clipsToBounds = true
+        cell.seller_name.text = String(self.jsonObject["user"][indexPath.item]["name"])
         
-        let url = NSURL(string: self.images[indexPath.item])
-        let data = NSData(contentsOfURL: url!)
-        cell.imageView?.image = UIImage(data: data!)**/
+        if (String(self.jsonObject["item"][indexPath.item]["user_id"]) == String(MyKeychainWrapper.myObjectForKey(kSecAttrAccount))) {
+            cell.title.text = "Renting To:"
+        }else {
+            cell.title.text = "Leasing From:"
+        }
         
-        //cell.imageView?.image = UIImageView(self.images[indexPath.item])
         return cell
     }
     
@@ -79,29 +91,15 @@ class TransactionViewController: UITableViewController {
         let urlString = "https://adae.co/api/v1/transactions/" + String(MyKeychainWrapper.myObjectForKey(kSecAttrAccount))
         
         Alamofire.request(.GET, urlString, headers: headers).response { (req, res, data, error) -> Void in
-            let json = JSON(data: data!)
+            //let json = JSON(data: data!)
             
-            for (_, object) in json {
-                self.items.append(String(object["item_id"]))
-                print(self.items)
-            }
+            self.jsonObject = JSON(data: data!)
             
-            for item in self.items {
-                let urlItem = "https://adae.co/api/v1/items/" + item
-                Alamofire.request(.GET, urlItem, headers: headers).response { (req, res, data, error) -> Void in
-                    let json = JSON(data: data!)
-                    
-                    self.images.append("https://adae.co/system/uploads/" + String(json["image"]))
-                    print(json)
-                    print(self.images)
-                    
-                    callback?(isOk: true)
-                    print("returned")
-                }
-                
-            }
+            print("JSONNN!!!!!!!!!!!")
+            print(self.jsonObject)
+            
+            callback?(isOk: true)
         }
         return "returned"
     }
-
 }
