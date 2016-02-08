3bff94f11cd4a95c61a7097a476d24cf3bf5e093
Atsushi Hirata <atsushi.hirata890@gmail.com>
2016-02-07 20:43:18 -0500

    in the details page, added description of item.

--- adae-ios-atsushi/TransactionDetailController.swift
+++ adae-ios-atsushi/TransactionDetailController.swift
@@ -22,11 +22,22 @@ class TransactionDetailController: UIViewController {
     
     @IBOutlet weak var statusLabel: UILabel!
     
+    let MyKeychainWrapper = KeychainWrapper()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
         let secondTab = self.tabBarController?.viewControllers![1] as! QRCodeViewController
         secondTab.toPass = self.toPass
+        
+        if String(self.toPass["transaction"]!["seller_id"]) == String(MyKeychainWrapper.myObjectForKey(kSecAttrAccount)) {
+            
+            requestTitle.text =  String(self.toPass["user"]!["name"]).capitalizedString + " would like to try"
+        }
+        
+        descriptionLabel.text = String(self.toPass["item"]!["description"])
+        
+        print(self.toPass)
         
         print("Inside of Transaction Detail Controller")
     }
